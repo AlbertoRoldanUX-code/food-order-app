d1530804a438b454a62d4fb85251a2b8e1df0432
unknown <a-f-r@hotmail.es>
2021-06-27 00:08:05 +0200

    Copy and paste css files from github

--- src/App.js
+++ src/App.js
@@ -9,3 +9,18 @@ function App() {
 }
 
 export default App
+
+
+
+// 1º Import css files from github.
+// 2º Create Header component.
+// 3º Create Cart component inside of header. 
+// 4º Create main description component.
+// 5º Create available meals with the dummy meals.
+// 6º Implement adding meals to the cart by clicking the Add button.
+// 7º Implement changing the amount of meals on AvailableMeals component.
+// 8º Implement opening modal when clicking on Cart.
+// 9º Implement changing the amount of meals on Cart component.
+// 10º Remove meal on Cart component if clicking on - and there's only 1 left.
+// 11º Console.log Ordering... when clicking on Order button from Cart component.
+// 12º Implement closing modal when clicking on Close button on Cart component.
