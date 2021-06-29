d6c1ec78746201e82579764365757d5394b0a564
unknown <a-f-r@hotmail.es>
2021-06-29 17:44:28 +0200

    Move Modal component to different div with Portals

--- src/components/Header/HeaderCartButton/Modal/Modal.js
+++ src/components/Header/HeaderCartButton/Modal/Modal.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import styles from './Modal.module.css';
 import Cart from './Cart/Cart.js';
 import Card from '../../../Card/Card';
@@ -14,12 +15,18 @@ function Modal(props) {
 
   return (
     <Fragment>
-      <Card>
-        <span className={styles.modal}>
-          <Cart meals={props.meals} onDelete={onDeleteHandler} />
-        </span>
-      </Card>
-      <div className={styles.backdrop} onClick={onClickHandler}></div>
+      {ReactDOM.createPortal(
+        <Card>
+          <span className={styles.modal}>
+            <Cart meals={props.meals} onDelete={onDeleteHandler} />
+          </span>
+        </Card>,
+        document.getElementById('overlays')
+      )}
+      {ReactDOM.createPortal(
+        <div className={styles.backdrop} onClick={onClickHandler}></div>,
+        document.getElementById('overlays')
+      )}
     </Fragment>
   );
 }
