0ae0a8880e2902963606f32463f8b1a76eff920d
unknown <a-f-r@hotmail.es>
2021-07-02 00:40:23 +0200

    Add cartReducer to provider component

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import CartContext from './cart-context';
+import { useReducer } from 'react';
+
+const cartReducer = function (state, action) {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  if (action.type === 'REMOVE') {
+    const updatedItems = state.items.filter((item) => item.id !== action.id);
+    return updatedItems;
+  }
+
+  return { items: [], totalAmount: 0 };
+};
 
 function CartProvider(props) {
-  const addItemToCartHandler = function (item) {};
-
-  const removeItemFromCartHandler = function (id) {};
-
-  const cartContext = {
+  const [cartState, dispatchCart] = useReducer(cartReducer, {
     items: [],
     totalAmount: 0,
+  });
+
+  const addItemToCartHandler = function (item) {
+    dispatchCart({ type: 'ADD', item: item });
+  };
+
+  const removeItemFromCartHandler = function (id) {
+    dispatchCart({ type: 'REMOVE', id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
