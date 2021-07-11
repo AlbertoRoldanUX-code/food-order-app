b61244ca664aed24ed6db7a024d7beecdc139ea5
unknown <a-f-r@hotmail.es>
2021-07-12 01:53:55 +0200

    Fetch the meals data from Firebase adding a GET request

--- src/components/AvailableMeals/AvailableMeals.js
+++ src/components/AvailableMeals/AvailableMeals.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../Card/Card';
 import styles from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
 function AvailableMeals() {
+  const [meals, setMeals] = useState([]);
+  const fetchMeals = async function () {
+    try {
+      const response = await fetch(
+        'https://react-http-484b3-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+      );
+      const data = await response.json();
+      setMeals(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
   return (
     <Card style={styles.meals}>
       <ul>
-        {DUMMY_MEALS.map((meal, index) => (
+        {meals.map((meal, index) => (
           <MealItem
             name={meal.name}
             description={meal.description}
