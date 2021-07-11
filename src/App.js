8440fd99ef5709aad315c2e40772a68fa3a393cc
unknown <a-f-r@hotmail.es>
2021-07-11 20:07:23 +0200

    Move dummy meals data to Firebase manually

--- src/App.js
+++ src/App.js
@@ -16,3 +16,12 @@ function App() {
 }
 
 export default App;
+
+//////////// 1º Use the same project from Firebase using the Realtime DB.
+//////////// 2º Clear any existing data you may have.
+//////////// 3º Move dummy meals data to Firebase manually.
+// 4º Add basic checkout form with name and address inputs and order button
+// 5º Validate user input before we submit it.
+// 6º Make sure that under rules, you have read and write set to true.
+// 7º Submit entered data to a backend with POST request
+// 8º Fetch the meals data from Firebase adding a GET request.
--- src/components/AvailableMeals/AvailableMeals.js
+++ src/components/AvailableMeals/AvailableMeals.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card';
 import styles from './AvailableMeals.module.css';
-import DUMMY_MEALS from './dummyMeals';
 import MealItem from './MealItem/MealItem';
 
 function AvailableMeals() {
